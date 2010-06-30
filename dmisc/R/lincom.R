ccced079c1620aec89a240bab79b19a8caea40c0
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-06-30 17:22:33 +0200

    add lincom facilities

---
+++ dmisc/R/lincom.R
@@ -0,0 +1,17 @@
+## linear combination of the columns of X,
+## eventually exploiting `names'
+lincom <- function(X, weights) {
+  if(!is.null(names(weights))) {
+    stopifnot(!is.null(colnames(X)))
+    X <- X[,names(weights)]
+  }
+  return(rowSums(sweep(X, 2, weights, "*")))
+}
+
+topN <- function(x, n, decreasing=FALSE) {
+  order(x, decreasing=decreasing)[seq_len(n)]
+}
+
+topNSign <- function(x, n, decreasing=FALSE) {
+  return(sign(x[topN(abs(x), n=n, decreasing=decreasing)]))
+}
