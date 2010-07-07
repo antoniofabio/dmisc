9074d59d6ee54b7b2e8f9bf851c4dac289b60b90
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-07-07 10:00:08 +0200

    add swapNamesAndContents manpage, fix typo in matrixTabular manpage

--- dmisc/R/tagsSystem.R
+++ dmisc/R/tagsSystem.R
@@ -3,9 +3,9 @@
 ##  and related manipulations
 ##
 ## convert an incidence matrix to a tags vector
-tagsFromIM <- function(IM) {
+tagsFromIM <- function(IM, sep=",") {
   tags <- colnames(IM)
-  return(apply(IM, 1, function(j) paste(tags[j], collapse=",")))
+  return(apply(IM, 1, function(j) collapse(tags[j], sep=sep)))
 }
 
 ## swap roles of 'names' and 'contents' of a vector 'x'
