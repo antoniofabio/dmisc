216f82fcd779c5a1e1349167d8f923e7f855b285
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2010-10-19 14:07:00 +0200

    improve args handling in 'fancyContrasts'

--- dmisc/R/fancyContrasts.R
+++ dmisc/R/fancyContrasts.R
@@ -1,4 +1,7 @@
 fancyContrasts <- function(x, base=1) {
+  if(is.character(base)) {
+    base <- match(base, levels(x))
+  }
   CC <- contr.treatment(levels(x), base=base)
   base <- rownames(CC)[base]
   colnames(CC) <- sprintf(" (%s vs %s)", colnames(CC), rep(base, NCOL(CC)))
